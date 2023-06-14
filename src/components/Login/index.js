9b67e176820ac7c52ed727ccc31a240bc8b097a4
Nannabeati <134772330+Nannabeati@users.noreply.github.com>
2023-06-14 22:23:54 +0530

    Update index.js Login component

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,13 +1,37 @@
 // Write your code here
+import {Component} from 'react'
 import './index.css'
 
-const Login = props => {
-  const {text} = props
-  return (
-    <button className="login-button" type="button">
-      {text}
-    </button>
-  )
+class Login extends Component {
+  state = {isLogin: true}
+
+  loginButton = () => {
+    this.setState(prevState => ({isLogin: !prevState.login}))
+  }
+
+  clickButton = () => {
+    const {isLogin} = this.state
+    if (isLogin === true) {
+      return (
+        <button
+          type="button"
+          className="login-button"
+          onClick={this.loginButton}
+        >
+          Login
+        </button>
+      )
+    }
+    return (
+      <button type="button" className="login-button" onClick={this.clickButton}>
+        Logout
+      </button>
+    )
+  }
+
+  render() {
+    return <div>{this.clickButton()}</div>
+  }
 }
 
 export default Login
