f332fdd594b97fb68bafca7dfc70fc8b82a568e7
Nannabeati <134772330+Nannabeati@users.noreply.github.com>
2023-06-14 22:28:03 +0530

    Update index.js Home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,24 +8,12 @@ import './index.css'
 class Home extends Component {
   state = {isLogin: true}
 
-  loginButton = () => {
-    this.setState(prevState => ({isLogin: !prevState.login}))
-  }
-
-  textMessage = () => {
-    const {isLogin} = this.state
-    if (isLogin === true) {
-      return <h1 className="heading">Please Login</h1>
-    }
-    return <Message />
-  }
-
   render() {
     const {isLogin} = this.state
 
     return (
       <div className="container">
-        {this.textMessage()}
+        <Message />
         {isLogin && <Login />}
         {!isLogin && <Logout />}
       </div>
