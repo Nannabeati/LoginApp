c99618477ca7d06ae11c5467bacbb217de8a30e6
Nannabeati <134772330+Nannabeati@users.noreply.github.com>
2023-06-14 22:26:39 +0530

    Update index.js Message component

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,9 +1,21 @@
 // Write your code here
+import {Component} from 'react'
 import './index.css'
 
-const Message = props => {
-  const {text} = props
-  return <h1 className="text-message">{text}</h1>
+class Message extends Component {
+  state = {isLogin: true}
+
+  textMessage = () => {
+    const {isLogin} = this.state
+    if (isLogin === true) {
+      return <h1 className="text-message">Please Login</h1>
+    }
+    return <h1 className="text-message">Welcome User</h1>
+  }
+
+  render() {
+    return <div>{this.textMessage()}</div>
+  }
 }
 
 export default Message
