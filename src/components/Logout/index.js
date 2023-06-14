b125300037c33d9e4be3a7b35a48d79300768182
Nannabeati <134772330+Nannabeati@users.noreply.github.com>
2023-06-14 22:25:21 +0530

    Update index.js  Logout

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -1,13 +1,39 @@
 // Write your code here
+import {Component} from 'react'
 import './index.css'
 
-const Logout = props => {
-  const {text} = props
-  return (
-    <button className="logout-button" type="button">
-      {text}
-    </button>
-  )
+class Logout extends Component {
+  state = {isLogin: true}
+
+  loginButton = () => {
+    this.setState(prevState => ({isLogin: !prevState.login}))
+  }
+
+  clickButton = () => {
+    const {isLogin} = this.state
+
+    if (isLogin === true) {
+      return (
+        <button
+          type="button"
+          className="login-button"
+          onClick={this.loginButton}
+        >
+          Login
+        </button>
+      )
+    }
+
+    return (
+      <button type="button" className="login-button" onClick={this.clickButton}>
+        Logout
+      </button>
+    )
+  }
+
+  render() {
+    return <div>{this.clickButton()}</div>
+  }
 }
 
 export default Logout
