2e0fe21ec8819ff7fb330f3ce8129449f5774e00
Nannabeati <134772330+Nannabeati@users.noreply.github.com>
2023-06-17 13:51:45 +0530

    added prop in logout

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -1,39 +1,20 @@
 // Write your code here
-import {Component} from 'react'
 import './index.css'
 
-class Logout extends Component {
-  state = {isLogin: true}
-
-  loginButton = () => {
-    this.setState(prevState => ({isLogin: !prevState.login}))
+const Logout = props => {
+  const {buttonEl, loginButton} = props
+  const logout = () => {
+    loginButton()
   }
+  return (
+    <button type="button" className="login-button" onClick={logout}>
+      {buttonEl}
+    </button>
+  )
+}
 
-  clickButton = () => {
-    const {isLogin} = this.state
-
-    if (isLogin === true) {
-      return (
-        <button
-          type="button"
-          className="login-button"
-          onClick={this.loginButton}
-        >
-          Login
-        </button>
-      )
-    }
-
-    return (
-      <button type="button" className="login-button" onClick={this.loginButton}>
-        Logout
-      </button>
-    )
-  }
-
-  render() {
-    return <div>{this.clickButton()}</div>
-  }
+Logout.defaultProps = {
+  buttonEl: 'Logout',
 }
 
 export default Logout
