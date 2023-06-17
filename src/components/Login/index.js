db329239b507c7502207fb4879765bc9a3c31c54
Nannabeati <134772330+Nannabeati@users.noreply.github.com>
2023-06-17 13:50:51 +0530

    added prop

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,37 +1,21 @@
 // Write your code here
-import {Component} from 'react'
 import './index.css'
 
-class Login extends Component {
-  state = {isLogin: true}
-
-  loginButton = () => {
-    this.setState(prevState => ({isLogin: !prevState.isLogin}))
+const Login = props => {
+  const {buttonEl, loginButton} = props
+  console.log(loginButton) // getting undefined in the console
+  const login = () => {
+    loginButton()
   }
+  return (
+    <button type="button" className="login-button" onClick={login}>
+      {buttonEl}
+    </button>
+  )
+}
 
-  clickButton = () => {
-    const {isLogin} = this.state
-    if (isLogin === true) {
-      return (
-        <button
-          type="button"
-          className="login-button"
-          onClick={this.loginButton}
-        >
-          Login
-        </button>
-      )
-    }
-    return (
-      <button type="button" className="login-button" onClick={this.loginButton}>
-        Logout
-      </button>
-    )
-  }
-
-  render() {
-    return <div>{this.clickButton()}</div>
-  }
+Login.defaultProps = {
+  buttonEl: 'Login',
 }
 
 export default Login
